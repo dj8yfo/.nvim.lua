0db3b691c95ee395874977378c39a6772d381a10
junkd social anxiety <djunxis@gmail.com>
2022-04-20 21:43:53 +0300

    navigation group; sayonara plugin step 2

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,6 +1,13 @@
 -- {{{ init
 local function init(use)
-    use 'mhinz/vim-sayonara'
+    use({
+	'mhinz/vim-sayonara',
+	setup = function()
+	    local map = require('settings.utils').map
+	    map('n', '<c-q>', [[<cmd>Sayonara<CR>]], { silent = true})
+	end,
+	cmd = 'Sayonara',
+    })
 end
 -- }}}
 return { init = init }
