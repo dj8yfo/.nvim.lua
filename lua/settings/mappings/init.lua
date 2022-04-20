3d548c5853b7025a6bcc2bbc3761d61f7795ad26
junkd social anxiety <djunxis@gmail.com>
2022-04-20 21:49:49 +0300

    map: <c-x> to :qa! <CR>

--- lua/settings/mappings/init.lua
+++ lua/settings/mappings/init.lua
@@ -1,6 +1,7 @@
-local map = vim.api.nvim_set_keymap
+local map = require('settings.utils').map
 
 
 local default_opts = {noremap = true, silent = true }
 
 map('n', '<F1>', ':nohl<CR>', default_opts)
+map('n', '<c-x>', ':qa!<CR>', default_opts)
