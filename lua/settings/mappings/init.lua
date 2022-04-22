82783c123045aff16726207b2f6d44ed5bd22a7c
junkd social anxiety <djunxis@gmail.com>
2022-04-22 22:34:22 +0300

    toggle wrap

--- lua/settings/mappings/init.lua
+++ lua/settings/mappings/init.lua
@@ -3,7 +3,6 @@ local map = require('settings.utils').map
 
 local default_opts = {noremap = true, silent = true }
 
-map('n', '<F1>', ':nohl<CR>', default_opts)
 map('n', '<c-x>', ':qa!<CR>', default_opts)
 
 map('n', '<m-j>', '<c-w>j', default_opts)
@@ -14,6 +13,10 @@ map('n', '<m-l>', '<c-w>l', default_opts)
 map('n', 'cp', ':let @" = expand("%")<cr>', default_opts)
 map('n', 'cP', ':let @" = expand("%:p")<cr>', default_opts)
 map('n', 'gw', ':w!<cr>', default_opts)
+map('n', 'gW', ':set wrap!<CR>', default_opts)
 
 map('n', 'gn', 'mzi<CR><Esc>`z', default_opts)
+
+
+
 
