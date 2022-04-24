34387793fd88bc7fc7447a6923c4403fdac3d5f4
junkd social anxiety <djunxis@gmail.com>
2022-04-24 22:25:26 +0300

    format zk plugin keybindings

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -8,14 +8,36 @@ if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
 
 	-- Create a new note after asking for its title.
 	-- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
-	map("n", "<leader>znn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
-	map("n", "<leader>zno", "<Cmd>ZkNotes<CR>", opts)
+	map(
+		"n", 
+		"<leader>znn", 
+		[[<Cmd>ZkNew { 
+			dir = vim.fn.expand('%:p:h'), 
+			title = vim.fn.input('Title: '),
+		}<CR>]], 
+		opts
+	)
 	-- Create a new note in the same directory as the current buffer, using the current selection for title.
-	map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", opts)
+	map(
+		"v", 
+		"<leader>znt", 
+		[[:'<,'>ZkNewFromTitleSelection { 
+			dir = vim.fn.expand('%:p:h'),
+		}<CR>]], 
+		opts
+	)
 	-- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
-	map("v", "<leader>znc", ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
-	map("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
+	map(
+		"v", "<leader>znc", 
+		[[:'<,'>ZkNewFromContentSelection { 
+			dir = vim.fn.expand('%:p:h'), 
+			title = vim.fn.input('Title: '),
+		}<CR>]], 
+		opts
+	)
 
+	map( "n", "<leader>zno", "<Cmd>ZkNotes<CR>", opts)
+	map("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
 	-- Open notes linking to the current buffer.
 	map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
 	-- Alternative for backlinks using pure LSP and showing the source context.
