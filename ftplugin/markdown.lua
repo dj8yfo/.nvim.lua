e268934ff05b11a0d24df644377c0a6acfc4dd83
junkd social anxiety <djunxis@gmail.com>
2022-04-25 17:08:00 +0300

    make key bindings oneline: ZkNewFromTitleSelection...

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -11,28 +11,20 @@ if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
 	map(
 		"n", 
 		"<leader>znn", 
-		[[<Cmd>ZkNew { 
-			dir = vim.fn.expand('%:p:h'), 
-			title = vim.fn.input('Title: '),
-		}<CR>]], 
+		[[<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>]], 
 		opts
 	)
 	-- Create a new note in the same directory as the current buffer, using the current selection for title.
 	map(
 		"v", 
 		"<leader>znt", 
-		[[:'<,'>ZkNewFromTitleSelection { 
-			dir = vim.fn.expand('%:p:h'),
-		}<CR>]], 
+		[[:'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>]], 
 		opts
 	)
 	-- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
 	map(
 		"v", "<leader>znc", 
-		[[:'<,'>ZkNewFromContentSelection { 
-			dir = vim.fn.expand('%:p:h'), 
-			title = vim.fn.input('Title: '),
-		}<CR>]], 
+		[[:'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>]], 
 		opts
 	)
 
