d7e1aeb1c1315be5f749a3f218ffa5f3d01a9ea0
junkd social anxiety <djunxis@gmail.com>
2022-04-26 00:09:02 +0300

    autocmd FileType go

---
+++ lua/settings/filetype/go/init.lua
@@ -0,0 +1,14 @@
+local exec = vim.api.nvim_exec
+exec(
+[[
+	augroup GoFiles
+	autocmd!
+    autocmd FileType go :set fp=gofmt
+    autocmd FileType go :iabbrev <buffer> s= :=
+    autocmd FileType go :iabbrev <buffer> n= !=
+    autocmd FileType go :iabbrev <buffer> zi, <-
+    autocmd FileType go :set shiftwidth=4
+    autocmd FileType go :set tabstop=4
+	augroup end
+]], false
+)
--- lua/settings/filetype/init.lua
+++ lua/settings/filetype/init.lua
@@ -1,3 +1,4 @@
 vim.cmd([[filetype plugin on]])
 
 require('settings.filetype.lua')
+require('settings.filetype.go')
