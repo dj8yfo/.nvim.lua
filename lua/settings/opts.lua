e7765fbb5d5f32ef6dcd9d1e7a202525fd5b617b
junkd social anxiety <djunxis@gmail.com>
2022-04-27 16:02:02 +0300

    add --fixed-strings to rg in grepprg

--- lua/settings/opts.lua
+++ lua/settings/opts.lua
@@ -6,7 +6,7 @@ local g = vim.g
 g.mapleader = [[ ]]
 vim.o.smartcase = true
 vim.o.ignorecase = true
-vim.o.grepprg = 'rg --vimgrep --no-heading --smart-case'
+vim.o.grepprg = 'rg -F --vimgrep --no-heading --smart-case'
 
 vim.o.splitright = true
 vim.o.splitbelow = false
