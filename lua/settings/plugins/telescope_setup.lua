d1130dad3f4504e1728674ba717de693f8ae32d7
junkd social anxiety <djunxis@gmail.com>
2022-04-28 02:50:56 +0300

    <c-h> -> Telescope frecency

--- lua/settings/plugins/telescope_setup.lua
+++ lua/settings/plugins/telescope_setup.lua
@@ -3,7 +3,7 @@ local map = require('settings.utils').map
 local silent = { silent = true }
 
 map('n', '<c-a>', [[<cmd>Telescope buffers show_all_buffers=true theme=get_ivy<cr>]], silent)
-map('n', '<c-r>', [[<cmd>Telescope frecency theme=get_ivy<cr>]], silent)
+map('n', '<c-h>', [[<cmd>Telescope frecency theme=get_ivy<cr>]], silent)
 
 map('n', '<c-s>', [[<cmd>Telescope git_files theme=get_ivy<cr>]], silent)
 map('n', '<c-,>', [[<cmd>Telescope find_files theme=get_ivy<cr>]], silent)
