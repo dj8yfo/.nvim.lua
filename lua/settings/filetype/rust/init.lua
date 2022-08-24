febdfb67f7b6d149830d383042ffc5214375585b
junkd social anxiety <djunxis@gmail.com>
2022-08-24 20:07:20 +0300

    rustfmt --edition

--- lua/settings/filetype/rust/init.lua
+++ lua/settings/filetype/rust/init.lua
@@ -3,7 +3,7 @@ exec(
 [[
 	augroup RustFiles
 	autocmd!
-    autocmd FileType rust :set fp=rustfmt
+    autocmd FileType rust :set fp=rustfmt\ --edition\ 2018
     autocmd FileType rust :iabbrev <buffer> n= !=
     autocmd FileType rust :iabbrev <buffer> pz __PLACE__
     autocmd FileType rust :set shiftwidth=4
