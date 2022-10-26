7e2b72a4f0dbae5868599bc3f8ba9228d5938e06
dj8yf0μl <sffhox@gmail.com>
2022-10-26 20:03:57 +0300

    tsserver added to lsp list

--- lua/settings/plugins/lspconfig/servers.lua
+++ lua/settings/plugins/lspconfig/servers.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.setup_servers(on_attach, capabilities) 
 	-- Use a loop to conveniently call 'setup' on multiple servers and
 	-- map buffer local keybindings when the language server attaches
-	local servers = {}
+	local servers = {'tsserver'}
 	for _, lsp in pairs(servers) do
 	  require('lspconfig')[lsp].setup {
 		on_attach = on_attach,
