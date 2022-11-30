2b2f49e5d37069d52244a81bc5a812d209504069
dj8yf0μl <sffhox@gmail.com>
2022-11-30 22:16:55 +0200

    push

--- lua/settings/filetype/rust/init.lua
+++ lua/settings/filetype/rust/init.lua
@@ -6,6 +6,7 @@ exec(
     autocmd FileType rust :set fp=rustfmt\ --edition\ 2018
     autocmd FileType rust :iabbrev <buffer> n= !=
     autocmd FileType rust :iabbrev <buffer> pz __PLACE__
+    autocmd FileType rust :iabbrev <buffer> ;; <'_>
     autocmd FileType rust :set shiftwidth=4
     autocmd FileType rust :set tabstop=4
 	augroup end
